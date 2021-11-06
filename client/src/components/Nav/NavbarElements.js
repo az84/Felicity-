92a3dded10bc2e9dac53439a886954422dc14d94
Lucas DiFalco <luke.difalco52@gmail.com>
2021-11-06 10:41:16 -0400

    deploying

---
+++ client/src/components/Nav/NavbarElements.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarComponents = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavbarComponents
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Felicity', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/felicity', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
